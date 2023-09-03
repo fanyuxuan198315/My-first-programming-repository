b37766491dc8e85b2f86fb7e9d9d28d396a20352
范昱轩 <126331654+fanyuxuan198315@users.noreply.github.com>
2023-09-03 16:40:02 +0800

    Add files via upload

---
+++ 判断英文字母.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+char temp = '0';
+if ((temp >= 'a' && temp <= 'z') || (temp >= 'A' && temp <= 'Z')) {
+	printf("yes");
+}
+else {
+	printf("no");
+
+	// insert code here...
+}
